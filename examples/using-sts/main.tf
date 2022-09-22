a45e44b6fdefa2d868ef3b0d48010a574a3b46e1
Pim Snel <post@pimsnel.com>
2022-09-22 05:23:33 +0200

    Update main.tf

--- examples/using-sts/main.tf
+++ examples/using-sts/main.tf
@@ -1,6 +1,6 @@
 module "linked-accounts-budget-alarms" {
   source  = "TechNative-B-V/linked-accounts-budget-alarms/aws"
-  version = "0.1.0"
+  version = "0.3.0"
 
   sts_master_account_role_arn = "arn:aws:iam::XXXXXXXXXXXX:role/MonitoringCostExplorer"
   slack_webhook_url = "https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXX/XXXXXXXXXXXXXXXXXXXXXXXX"
